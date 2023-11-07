c4e11bb842066414555a41d60dee327a48ed009f
tylersmith <tyjsmith1@gmail.com>
2023-11-07 11:52:32 -0600

    liker

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
+// const { returns } = require("chai-spies")
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+// Your JavaScript code goes here!
+const hearts = document.querySelectorAll("span.like-glyph")
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    mimicServerCall(e)
+    .then(response => {
+      console.log('success')
+    document.getElementById("modal").classList.add("hidden")
+      if(heart.innerHTML === EMPTY_HEART) {
+        heart.innerHTML = FULL_HEART
+        heart.classList.add("activated-heart")
+      } else if(heart.innerHTML === FULL_HEART) {
+        heart.innerHTML = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+
+    })
+    .catch(error => {
+      console.log('error')
+      document.getElementById("modal").classList.remove("hidden")
+      setTimeout(() => {
+        document.getElementById("modal").classList.add("hidden")
+      }, 3000)
+    })
+  })
+})
 
 
 
